4c77f7431aff80d91f41e1b6f2ec128d6b6418e1
Nhat Nguyen <nhatnx@gmail.com>
2026-06-29 00:26:59 +0700

    feat: add allowedDevOrigins to next.config for development access

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: process.env.BUILD_STANDALONE === "1" ? "standalone" : undefined,
+  allowedDevOrigins: ["192.168.86.32"],
 };
 
 export default nextConfig;
